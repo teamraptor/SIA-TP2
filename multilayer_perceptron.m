0d3a77da94321cad52d67bf8b8be5087df0ca099
lmarzora <lmarzora@itba.edu.ar>
2017-05-06 20:16:19 -0300

    multilayer

--- multilayer_perceptron.m
+++ multilayer_perceptron.m
@@ -3,7 +3,7 @@ function output = get_output(entry,weights,activation_func)
     output = activation_func(weights * entry);
 end
 
-function [weights,output] = multilayer_perceptron_learn(entries, expected_output, neurons_per_layer, activation_func, activation_der,
+function [weights,output,error] = multilayer_perceptron_learn(entries, expected_output, neurons_per_layer, activation_func, activation_der,
                                     learning_factor=.5, max_iterations=1000, tolerance=1e-5)
     n = length(entries(:, 1));
     m = 0;
@@ -12,7 +12,7 @@ function [weights,output] = multilayer_perceptron_learn(entries, expected_output
         weights{m} = rand(neurons_per_layer(i), neurons_per_layer(i-1)+1) .- 0.5;
         layer_entry{m} = [-1, zeros(1, neurons_per_layer(i-1))];
     end
-    M = m
+    M = m;
     for iteration = 1:max_iterations
         for index = randperm(2^n); 
                 layer_entry{1}(2:end) = entries(:, index);
@@ -24,7 +24,7 @@ function [weights,output] = multilayer_perceptron_learn(entries, expected_output
             output(index);
             d{M} = activation_der(weights{M}*layer_entry{M}')*(expected_output(index) - output(index));
             for i = M-1:-1:1
-                d{i} = activation_der(weights{i}*layer_entry{i}')*(weights{i+1}(:,2:end)*d{i+1});
+                d{i} = (weights{i+1}(:,2:end)*d{i+1}' * activation_der(weights{i}*layer_entry{i}'));
             end
             d;
             weights;
@@ -33,8 +33,8 @@ function [weights,output] = multilayer_perceptron_learn(entries, expected_output
                 weights{i} = weights{i} + delta_w';
             end
         end
-        sum(abs(expected_output - output))
-        if sum(abs(expected_output - output)) <= tolerance
+        error(iteration) = sum(abs(expected_output - output));
+        if error <= tolerance
             return
         end
     end
