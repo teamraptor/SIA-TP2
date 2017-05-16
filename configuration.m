a45570c41a50d9cb20aa704ab54e0a3e76ac8c3f
Lucas Soncini <lsoncini@itba.edu.ar>
2017-05-16 19:21:19 -0300

    Configuration style

--- configuration.m
+++ configuration.m
@@ -1,4 +1,6 @@
 15;
+
+% Parameters
 data_file = 'terrain/terrain03.data';
 starting_line = 2;
 beta = 15;
@@ -7,7 +9,11 @@ net = [2, 45, 50, 1];
 eta = 0.1;
 max_iterations = 1e2;
 cut_error = 1e-4;
+
+% Optimizations
 alpha = 0;
 adaptative_eta = true;
+
+% Display
 print_error = true;
 print_estimation = false;
